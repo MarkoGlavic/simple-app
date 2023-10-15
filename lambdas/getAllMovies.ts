d57a1f258ddb0d3e0a6fcf0cb9e7407dc0832f56
Marko Glavic <glavim@gmail.com>
2023-10-15 18:26:05 +0100

    Lambda function to get all movies

--- lambdas/getAllMovies.ts
+++ lambdas/getAllMovies.ts
@@ -1,6 +1,6 @@
 import { Handler } from "aws-lambda";
 
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
@@ -9,25 +9,15 @@ export const handler: Handler = async (event, context) => {
   try {
     console.log("Event: ", event);
     const parameters = event?.queryStringParameters;
-    const movieId = parameters ? parseInt(parameters.movieId) : undefined;
 
-    if (!movieId) {
-      return {
-        statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Missing movie Id" }),
-      };
-    }
+
     const commandOutput = await ddbDocClient.send(
-      new GetCommand({
-        TableName: process.env.TABLE_NAME,
-        Key: { movieId: movieId },
+      new ScanCommand({
+            TableName: process.env.TABLE_NAME,
       })
     );
     console.log('GetCommand response: ', commandOutput)  // NEW
-    if (!commandOutput.Item) {
+    if (!commandOutput.Items) {
       return {
         statusCode: 404,
         headers: {
@@ -37,7 +27,7 @@ export const handler: Handler = async (event, context) => {
       };
     }
     const body = {
-      data: commandOutput.Item,
+      data: commandOutput.Items,
     };
 
     // Return Response
